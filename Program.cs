f2f921bc34dd81e0d426a4d465934152e3f2805e
srwesteban <sr.w@hotmail.com>
2022-10-01 16:17:17 -0500

    sdf

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EjercicioEmpleado
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Empleado empleado = new Empleado();
+
+            Console.WriteLine(empleado);
+
+            Secretario secretario = new Secretario();
+            Console.WriteLine(secretario);
+            
+
+        }
+
+    }
+  
+}
